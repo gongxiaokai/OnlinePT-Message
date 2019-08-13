e6c3aa3efc2ea45043524c740f73ef4adb92eed8
gongxiaokai <139391025@qq.com>
2019-08-13 14:12:51 +0800

    [Fix] fix git

--- OnlinePT-Message/Classes/Common/Reply/ReplyVC.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ReplyVC.swift
-//  OnlinePT-Message
-//
-//  Created by gongwenkai on 2019/8/12.
-//
-
-import Foundation
--- OnlinePT-Message/Classes/Common/Reply/Subviews/ReplyCommentView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ReplyCommentView.swift
-//  OnlinePT-Message
-//
-//  Created by gongwenkai on 2019/8/12.
-//
-
-import Foundation
